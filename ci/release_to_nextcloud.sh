24bc438d5cb46dc125dd3defd8fd7a3de77ce77a
Janis Köhr <janis.koehr@novatec-gmbh.de>
2019-07-03 21:43:11 +0200

    Fixed app_version reference in ci script.

--- ci/release_to_nextcloud.sh
+++ ci/release_to_nextcloud.sh
@@ -5,4 +5,6 @@ rm -f signing.key
 
 app_version=$(cat new_version)
 
+echo "Releasing the app v$app_version to the nextcloud app store."
+
 curl -X POST -H 'Content-Type: application/json' -H "Authorization: Token $nextcloud_appstore_token" -d '{ "download": "https://github.com/janis91/ocr/releases/download/'"$app_version"'/ocr.tar.gz", "signature": "'"$SIGNATURE"'" }' https://apps.nextcloud.com/api/v1/apps/releases
