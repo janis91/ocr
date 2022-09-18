b2b044beeb8f037b4e511673f4b4d90e5aae91f8
rakekniven <2069590+rakekniven@users.noreply.github.com>
2022-09-18 18:44:49 +0200

    l10n: Fixed grammar
    Reported at Transifex.
    
    Signed-off-by: rakekniven <2069590+rakekniven@users.noreply.github.com>

--- src/app/configuration/Translations.ts
+++ src/app/configuration/Translations.ts
@@ -1,9 +1,9 @@
 export class Translations {
     public static TRANSLATION_TARGET_FILE_ALREADY_EXISTS: string = t('ocr', 'Target file already exists:');
-    public static TRANSLATION_UNEXPECTED_ERROR_LOAD_FAVORITE_LANGUAGES: string = t('ocr', 'An unexpected error occured during the load of your favorite languages. No language will be set instead.');
-    public static TRANSLATION_UNEXPECTED_ERROR_UPLOAD: string = t('ocr', 'An unexpected error occured during the upload of the processed file.');
-    public static TRANSLATION_UNEXPECTED_ERROR_DELETION: string = t('ocr', 'An unexpected error occured during the deletion of the original file.');
-    public static TRANSLATION_UNEXPECTED_ERROR_TESSERACT_PROCESSING: string = t('ocr', 'An unexpected error occured during Tesseract processing.');
+    public static TRANSLATION_UNEXPECTED_ERROR_LOAD_FAVORITE_LANGUAGES: string = t('ocr', 'An unexpected error occurred during the load of your favorite languages. No language will be set instead.');
+    public static TRANSLATION_UNEXPECTED_ERROR_UPLOAD: string = t('ocr', 'An unexpected error occurred during the upload of the processed file.');
+    public static TRANSLATION_UNEXPECTED_ERROR_DELETION: string = t('ocr', 'An unexpected error occurred during the deletion of the original file.');
+    public static TRANSLATION_UNEXPECTED_ERROR_TESSERACT_PROCESSING: string = t('ocr', 'An unexpected error occurred during Tesseract processing.');
     public static TRANSLATION_OCR_PROCESSING_FAILED: string = t('ocr', 'OCR processing failed:');
     public static TRANSLATION_MIMETYPE_NOT_SUPPORTED: string = t('ocr', 'MIME type not supported.');
     public static TRANSLATION_PROCESS: string = t('ocr', 'Process');
